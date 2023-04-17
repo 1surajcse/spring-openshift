a41f017e2f7b4d6636b1cdb1854b2c77025b19b4
Suraj Tiwari <1surajcse@gmail.com>
2023-04-17 13:21:56 +0530

    deleted the older files and folder

--- src/main/java/com/validation/com/validationdemo/exception/IncorrectDataFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.validation.com.validationdemo.exception;
-
-public class IncorrectDataFoundException extends Exception{
-
-    public IncorrectDataFoundException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/validation/com/validationdemo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.validation.com.validationdemo.repository;
-
-import com.validation.com.validationdemo.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
